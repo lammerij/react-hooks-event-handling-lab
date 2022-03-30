2b99265f419009b009dc222c1d7bafe4616384f8
+ <+>
2022-03-30 12:40:39 -0400

    eyesonme

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
-// Code EyesOnMe Component Here
+import React from "react"
+
+function EyesOnMe()
+    function Focus(){
+        
+    }
+{
+    return (
+        <button> "Eyes on me"</button>
+    )
+}
+
+export default EyesOnMe;
